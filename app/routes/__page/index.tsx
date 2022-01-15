4f45966ab55114288a17344cfb70c3fd6aa1b747
dsod <d.soderling@live.se>
2022-01-15 21:14:10 +0100

    More clean solution in the __pages/index.tsx

--- app/routes/__page/index.tsx
+++ app/routes/__page/index.tsx
@@ -1,8 +1,2 @@
 // This file simply exist so that the "" and "/" routes will render the same component as the other pages
-import Page, { loader } from './$page'
-export {loader}
-export default function PageIndex() {
-  return (
-    <Page />
-  )
-}
+export {loader, default} from "./$page"
